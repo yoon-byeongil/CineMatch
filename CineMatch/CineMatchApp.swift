119fe55757de1695a8fde8e071930384a2246dbb
yoon-byeongil <justbrand101@gmail.com>
2026-04-22 12:04:54 +0900

    Create Project and Folder

---
+++ CineMatch/CineMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineMatchApp.swift
+//  CineMatch
+//
+//  Created by 윤병일 on 2026/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CineMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
